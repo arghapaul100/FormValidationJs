3f9e23428c12805119412e21fd30e8ff62353028
Argha <argha@webcreatoredigitalsolutions.com>
2022-04-08 01:03:32 +0530

    updated git-push.sh file

--- git-push.sh
+++ git-push.sh
@@ -6,6 +6,7 @@ git status
 echo -n "Please enter a commit message : "
 read message
 git commit -m "${message}"
+echo "Updating changes to the remote repository..."
 git push origin master 
 echo "Git Push Successfuly Completed"
-sleep 3s
+sleep 2s
