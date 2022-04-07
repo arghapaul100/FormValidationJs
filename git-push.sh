9e014e11f220c1d50aba141b6393df3b796dc8f6
Argha <argha@webcreatoredigitalsolutions.com>
2022-04-08 00:55:00 +0530

    8-4-2022 00:54 AM

--- git-push.sh
+++ git-push.sh
@@ -3,9 +3,9 @@ git status
 git add .
 echo "Status After Adding Files To The Staging Area"
 git status
-echo "Please enter a commit message"
+echo -n "Please enter a commit message : "
 read message
 git commit -m "${message}"
 git push origin master 
 echo "Git Push Successfuly Completed"
-read
+sleep 5s
