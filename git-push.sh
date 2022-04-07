cb955981a1f237a17f99b2186701f32fbc103e0b
Argha <argha@webcreatoredigitalsolutions.com>
2022-04-08 01:06:32 +0530

    updated git-push.sh file

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,12 @@
-echo "Status Before Adding Files To The Staging Area"
+echo "Status before adding files to the staging area : "
 git status
 git add .
-echo "Status After Adding Files To The Staging Area"
+echo "Status after adding files to the staging area : "
 git status
 echo -n "Please enter a commit message : "
 read message
 git commit -m "${message}"
-echo "Updating changes to the remote repository..."
+echo "Please wait a while! updating changes to the remote repository..."
 git push origin master 
-echo "Git Push Successfuly Completed"
+echo "Git Push Successfuly Completed."
 sleep 2s
